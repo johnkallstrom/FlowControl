5384d85f6e95b2ea370050da6e3a5b73035d60fd
John Källström <johnkaellstrom@gmail.com>
2024-03-01 11:45:07 +0100

    Display main menu with title, descr etc and a loop for running the program

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -4,8 +4,34 @@
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Flow control");
-			Console.ReadKey();
+			string title = "Flow control";
+			string description = "Here is the main menu, use numbers to select an option";
+			string[] options = ["1. Youth or senior", "0. Exit"];
+
+			while (true)
+			{
+				UserInterface.ShowMenu(title, description, options);
+
+				string input = UserInterface.GetStringInput("Enter: ");
+				switch (input)
+				{
+					case "0":
+						Environment.Exit(0);
+						break;
+					case "1":
+						Console.Clear();
+                        Console.WriteLine("Youth or senior");
+                        Console.WriteLine();
+
+						int age = UserInterface.GetIntInput("Age: ");
+
+                        Console.ReadKey();
+                        break;
+					default:
+						UserInterface.Text($"Incorrect input", withDots: true);
+						break;
+				}
+			}
         }
 	}
 }
