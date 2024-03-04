06f895537f49a0f8377f1496e03b75284405c475
John Källström <johnkaellstrom@gmail.com>
2024-03-04 15:30:01 +0100

    Third word menu option

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -8,12 +8,12 @@ namespace FlowControl
 		{
 			string title = "Flow control";
 			string description = "Here is the main menu, use numbers to select an option";
-			string[] options = ["1. Youth or senior", "2. Calculate total price", "3. Repeat ten times", "0. Exit"];
+			string[] options = ["1. Youth or senior", "2. Calculate total price", "3. Repeat ten times", "4. The third word", "0. Exit"];
 
 			while (true)
 			{
 				UI.CreateMenu(title, description, options);
-				int selection = InputReader.GetInteger(min: 0, max: 3, "Enter: ");
+				int selection = InputReader.GetInteger(min: 0, max: 4, "Enter: ");
 				switch (selection)
 				{
 					case 0:
@@ -106,6 +106,33 @@ namespace FlowControl
                             }
 
 							UI.Space();
+							UI.CreateMenu(["1. Try again", "0. Back to main menu"]);
+							selection = InputReader.GetInteger(min: 0, max: 1, "Enter: ");
+
+							if (selection is 1) continue;
+							if (selection is 0) break;
+						}
+						break;
+					case 4:
+						while (true)
+						{
+							UI.Clear();
+							UI.Text("The third word");
+							UI.Text("Write a sentence with atleast three words");
+							UI.Space();
+
+							string text = InputReader.GetString("Enter text: ");
+							var words = text.Split(" ");
+
+							if (words.Count() >= 3)
+							{
+								UI.Text(words[2]);
+							}
+							else
+							{
+								UI.Text("Not enough words");
+							}
+
 							UI.CreateMenu(["1. Try again", "0. Back to main menu"]);
 							selection = InputReader.GetInteger(min: 0, max: 1, "Enter: ");
 
