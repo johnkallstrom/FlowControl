f527202e4692612a8446afd8ab920af2ca977950
John Källström <johnkaellstrom@gmail.com>
2024-03-04 12:20:00 +0100

    Class for reading inputs

---
+++ FlowControl/InputReader.cs
@@ -0,0 +1,53 @@
+﻿namespace FlowControl
+{
+	internal static class InputReader
+	{
+		public static string GetString(string msg = "")
+		{
+			while (true)
+			{
+				Console.Write(msg);
+				string? input = Console.ReadLine();
+
+				if (!string.IsNullOrWhiteSpace(input))
+				{
+					return input;
+				}
+			}
+		}
+
+		public static int GetInteger(string msg = "")
+		{
+			while (true)
+			{
+				Console.Write(msg);
+				string? input = Console.ReadLine();
+
+				if (int.TryParse(input, out int result))
+				{
+					if (result > 0)
+					{
+						return result;
+					}
+				}
+			}
+		}
+
+		public static int GetInteger(int min, int max, string msg = "")
+		{
+			while (true)
+			{
+				Console.Write(msg);
+				string? input = Console.ReadLine();
+
+				if (int.TryParse(input, out int result))
+				{
+					if (result >= min && result <= max)
+					{
+						return result;
+					}
+				}
+			}
+		}
+	}
+}
