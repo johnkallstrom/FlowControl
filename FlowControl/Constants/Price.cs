b72fbeffa3ca711dc27d4e27f91b97a81f51f266
John Källström <johnkaellstrom@gmail.com>
2024-03-05 09:40:35 +0100

    Price constanss for children and elder ppl

--- FlowControl/Constants/Price.cs
+++ FlowControl/Constants/Price.cs
@@ -2,8 +2,10 @@
 {
 	internal class Price
 	{
+		public const int Child = 0;
 		public const int Youth = 80;
 		public const int Senior = 90;
+		public const int Ancient = 0;
 		public const int Standard = 120;
 	}
 }
